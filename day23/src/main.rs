589b7929333f6e871b7623da1acffac4529c96a6
Rowan Hill <rowan.hill@softwire.com>
2016-12-23 09:16:26 +0000

    Day 23: rewrite with &str everywhere and array for registers - much faster

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,29 +1,22 @@
 extern crate regex;
 use regex::Regex;
 
-use std::collections::HashMap;
-
 use Instruction::*;
 
 #[derive(Debug, Clone, Copy)]
 enum Instruction<'a> {
-    CopyVal{value:i32, target:&'a str},
-    CopyReg{source:&'a str, target:&'a str},
+    Copy{source:&'a str, target:&'a str},
     Inc{reg:&'a str},
     Dec{reg:&'a str},
-    JumpRegNotZero{check:&'a str, delta:&'a str},
-    JumpValNotZero{value: i32, delta:&'a str},
+    JumpNotZero{check:&'a str, delta:&'a str},
     Toggle{reg:&'a str}
 }
 
 impl<'a> Instruction<'a> {
     fn toggle(&self) -> Instruction<'a> {
         match *self {
-            CopyVal{value, target} => {
-                JumpValNotZero{value: value, delta: target}
-            },
-            CopyReg{source, target} => {
-                JumpRegNotZero{check: source, delta: target}
+            Copy{source, target} => {
+                JumpNotZero{check: source, delta: target}
             },
             Inc{reg} => {
                 Dec{reg: reg}
@@ -31,11 +24,8 @@ impl<'a> Instruction<'a> {
             Dec{reg} => {
                 Inc{reg: reg}
             },
-            JumpValNotZero{value, delta} => {
-                CopyVal{value: value, target: delta}
-            },
-            JumpRegNotZero{check, delta} => {
-                CopyReg{source: check, target: delta}
+            JumpNotZero{check, delta} => {
+                Copy{source: check, target: delta}
             },
             Toggle{reg} => {
                 Inc { reg: reg }
@@ -44,29 +34,22 @@ impl<'a> Instruction<'a> {
     }
 }
 
-fn main() {
+fn parse(file: &str) -> Vec<Instruction> {
     let cpy_re:Regex = Regex::new(r"cpy (.+?) (.+)").unwrap();
     let inc_re = Regex::new(r"inc (.+?)").unwrap();
     let dec_re = Regex::new(r"dec (.+?)").unwrap();
     let jnz_re = Regex::new(r"jnz (.+?) (.+)").unwrap();
     let tgl_re = Regex::new(r"tgl (.+?)").unwrap();
 
-    let file = include_str!("input.txt");
     let mut instructions = vec![];
 
     for line in file.lines() {
         let instr = if let Some(caps) = cpy_re.captures(line) {
             let val_or_reg = caps.at(1).unwrap();
             let target_reg = caps.at(2).unwrap();
-
-            if let Ok(val) = val_or_reg.parse::<i32>() {
-                CopyVal{value: val, target: target_reg}
-            } else {
-                CopyReg{source: val_or_reg, target: target_reg}
-            }
+            Copy{source: val_or_reg, target: target_reg}
         } else if let Some(caps) = inc_re.captures(line) {
             let reg = caps.at(1).unwrap();
-
             Inc{reg: reg}
         } else if let Some(caps) = dec_re.captures(line) {
             let reg = caps.at(1).unwrap();
@@ -74,12 +57,7 @@ fn main() {
         } else if let Some(caps) = jnz_re.captures(line) {
             let val_or_reg = caps.at(1).unwrap();
             let delta = caps.at(2).unwrap();
-
-            if let Ok(val) = val_or_reg.parse::<i32>() {
-                JumpValNotZero{value: val, delta: delta}
-            } else {
-                JumpRegNotZero{check: val_or_reg, delta: delta}
-            }
+            JumpNotZero{check: val_or_reg, delta: delta}
         } else if let Some(caps) = tgl_re.captures(line) {
             let reg = caps.at(1).unwrap();
             Toggle{reg: reg}
@@ -90,73 +68,86 @@ fn main() {
         instructions.push(instr);
     }
 
-    let mut regs = HashMap::new();
-    regs.insert("a", 12i32); // 7 for part 1, 12 for pqrt 2
-    regs.insert("b", 0);
-    regs.insert("c", 0);
-    regs.insert("d", 0);
+    instructions
+}
 
-    let mut instr_idx = 0i32;
-    while instr_idx < instructions.len() as i32 {
-        let instr = instructions[instr_idx as usize];
-        match instr {
-            CopyVal { value, target } => {
-                regs.insert(target, value);
-//                println!("Inserting {} to {}", value, target);
-            },
-            CopyReg { source, target } => {
-                let value = *regs.get(source).expect(&format!("Unknown register: '{}'", source));
-                regs.insert(target, value);
-//                println!("Inserting {} (from {}) to {}", value, source, target);
-            },
-            Inc { reg } => {
-                let mut val = regs.get_mut(reg).unwrap();
-                *val += 1;
-//                println!("Incrementing {} to {}", reg, val);
-            },
-            Dec { reg } => {
-                let mut val = regs.get_mut(reg).unwrap();
-                *val -= 1;
-//                println!("Decrementing {} to {}", reg, val);
-            },
-            JumpValNotZero { value, delta } => {
-                let delta = if let Ok(d) = delta.parse::<i32>() {
-                    d
-                } else {
-                    *regs.get(&delta).unwrap()
-                };
-                if value != 0 {
-                    instr_idx += delta;
-//                    println!("Jumping by {} to {}", delta, instr_idx);
-                    continue;
-                }
-            },
-            JumpRegNotZero { check, delta } => {
-                let delta = if let Ok(d) = delta.parse::<i32>() {
-                    d
-                } else {
-                    *regs.get(&delta).unwrap()
-                };
-                let value = *regs.get(check).expect(&format!("Unknown register: '{}'", check));
-                if value != 0 {
-                    instr_idx += delta;
-//                    println!("Jumping by {} to {}", delta, instr_idx);
-                    continue;
-                }
-            },
-            Toggle { reg } => {
-                let delta = *regs.get(reg).expect(&format!("Unknown register: '{}'", reg));
-                let idx = instr_idx + delta;
-                if idx >= 0 && (idx as usize) < instructions.len() {
-                    let toggled = instructions[idx as usize].toggle();
+struct Cpu {
+    regs: [i32; 4]
+}
 
-                    instructions[idx as usize] = toggled;
+impl Cpu {
+    fn new(a_val: i32) -> Cpu {
+        Cpu {
+            regs: [a_val, 0, 0, 0]
+        }
+    }
+
+    fn reg_idx(reg: &str) -> usize {
+        match reg {
+            "a" => 0,
+            "b" => 1,
+            "c" => 2,
+            "d" => 3,
+            _   => unreachable!("Unexpected register name: {}", reg)
+        }
+    }
+
+    fn value(&self, reg_or_val: &str) -> i32 {
+        match reg_or_val {
+            "a" | "b" | "c" | "d" => self.regs[Self::reg_idx(reg_or_val)],
+            _ => reg_or_val.parse::<i32>().unwrap()
+        }
+    }
+
+    fn set_value(&mut self, reg: &str, reg_or_val: &str) {
+        let value = self.value(reg_or_val);
+        self.regs[Self::reg_idx(reg)] = value;
+    }
+
+    fn process(&mut self, mut instructions: Vec<Instruction>) {
+        let mut instr_idx = 0i32;
+        while instr_idx < instructions.len() as i32 {
+            let instr = instructions[instr_idx as usize];
+            match instr {
+                Copy { source, target } => {
+                    self.set_value(target, source);
+                },
+                Inc { reg } => {
+                    self.regs[Self::reg_idx(reg)] = self.value(reg) + 1;
+                },
+                Dec { reg } => {
+                    self.regs[Self::reg_idx(reg)] = self.value(reg) - 1;
+                },
+                JumpNotZero { check, delta } => {
+                    let value = self.value(check);
+                    if value != 0 {
+                        instr_idx += self.value(delta);
+                        continue;
+                    }
+                },
+                Toggle { reg } => {
+                    let idx = instr_idx + self.value(reg);
+                    if idx >= 0 && (idx as usize) < instructions.len() {
+                        let toggled = instructions[idx as usize].toggle();
+
+                        instructions[idx as usize] = toggled;
+                    }
                 }
             }
-        }
 
-        instr_idx += 1;
-//        println!("Tick! Counter: {} Regs: {:?}", instr_idx, regs);
+            instr_idx += 1;
+        }
     }
-    println!("{:?}", regs);
+}
+
+fn main() {
+    let instructions = parse(include_str!("input.txt"));
+
+    let mut cpu = Cpu::new(7);
+    cpu.process(instructions.clone());
+    println!("{:?}", cpu.regs);
+
+    let mut cpu2 = Cpu::new(12);
+    cpu2.process(instructions);
+    println!("{:?}", cpu2.regs);
 }
